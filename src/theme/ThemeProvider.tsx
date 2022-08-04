ae2ed35dd373df54fd5f6b261251d2d32cfb5119
tvbthien <tvbthien@gmail.com>
2022-08-04 10:42:29 +0700

    First commit

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { themeCreator } from './base';
+import { StylesProvider } from '@mui/styles';
+
+export const ThemeContext = React.createContext(
+  (themeName: string): void => {}
+);
+// type BoxProps = {
+//   children: React.ReactNode; // 👈️ type children
+// };
+const ThemeProviderWrapper: React.FC<{ children: React.ReactNode }> = (props: any) => {
+  const curThemeName = localStorage.getItem('appTheme') || 'PureLightTheme';
+  const [themeName, _setThemeName] = useState(curThemeName);
+  const theme = themeCreator(themeName);
+  const setThemeName = (themeName: string): void => {
+    localStorage.setItem('appTheme', themeName);
+    _setThemeName(themeName);
+  };
+
+  return (
+    <StylesProvider injectFirst>
+      <ThemeContext.Provider value={setThemeName}>
+        <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+      </ThemeContext.Provider>
+    </StylesProvider>
+  );
+};
+
+export default ThemeProviderWrapper;
